db7db110d458420a6c1fab61f6f292be6d29eaa0
tangczc <mytczc521@outlool.com>
2018-10-29 20:37:46 +0800

    bbs

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,7 +1,5 @@
 class PageController < ApplicationController
   def welcome
-  end
-  def about
-  	
+  	 @issues  = [ { title: "Ruby lovers", comments: "4" }, { title: "Rails and Laravel", comments: "5" } ]
   end
 end
