f83a7318bc1d4ae06d0a84b5485627f1d1f94cc9
tangczc <Mytczc521@outlook.com>
2018-11-19 19:59:06 +0800

    db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_17_065206) do
+ActiveRecord::Schema.define(version: 2018_11_19_102936) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_11_17_065206) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "content"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
